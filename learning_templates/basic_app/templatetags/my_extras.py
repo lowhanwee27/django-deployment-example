4cd0f7e66b5f8c5c8571bbe633a4b62d25cf0070
henry low <hwlow@myetrade.com.my>
2018-04-23 17:38:49 +0800

    asdfasfa

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -2,7 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.fiter(name='cut')
+@register.filter(name='cut')
 def cut(value,arg):
     """
     this cuts out alkl values of arg from the string !
